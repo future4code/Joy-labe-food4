50ff5d6f14d2b6117790828f8886e8ce859c7eb9
FredNeves95 <fredbneves95@gmail.com>
2022-02-16 21:05:56 -0300

    página de cadastro de usuário corrigida

--- src/page/SingUpPage/SignUp.js
+++ src/page/SingUpPage/SignUp.js
@@ -1,69 +1,18 @@
 import React from 'react'
-import styled from 'styled-components'
 import logo from "../../images/logo-future-eats-invert.png"
 import { useState } from 'react';
 
-import { Box } from '@mui/material';
+
 import TextField from '@mui/material/TextField';
 import { InputAdornment } from '@mui/material';
-import { InputLabel } from '@mui/material';
-import { FormControl } from '@mui/material';
-import { OutlinedInput } from '@mui/material';
 import { IconButton } from '@mui/material';
 import { Visibility } from '@mui/icons-material';
 import { VisibilityOff } from '@mui/icons-material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
-const SignUpStyle = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
+import { SignUpStyle } from './styled';
+import { Header } from './styled';
 
-img{
-    margin: 16px auto 16px auto;
-    width: 104px;
-    height: 58px;
-}
-
-#button--criar{
-    width: 328px;
-    height: 43px;
-    padding: 12px 16px;
-    border-radius: 2px;
-    background-color: #e86e5a;
-    border: none;
-
-}
-
-h4{
-    width: 296px;
-    height: 18px;
-    font-size: 16px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    text-align: center;
-    color: #000;
-}
-`
-const Header = styled.div`
-width: 100%;
-height: 64px;
-box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);
-
-.icon{
-    width: 23px;
-    height: 24px;
-    margin: 30px auto 10px 16px;
-}
-
-.input{
-    width: 328px;
-    height: 56px;
-}
-
-`
 
 const SignUp = () => {
     const [values, setValues] = React.useState({
@@ -73,6 +22,7 @@ const SignUp = () => {
         weight: '',
         weightRange: '',
         showPassword: false,
+        showConfirmPassword: false,
     });
 
     const handleChange = (prop) => (event) => {
@@ -83,6 +33,13 @@ const SignUp = () => {
         setValues({
             ...values,
             showPassword: !values.showPassword,
+        });
+    };
+
+    const handleClickShowConfirmPassword = () => {
+        setValues({
+            ...values,
+            showConfirmPassword: !values.showConfirmPassword,
         });
     };
 
@@ -130,24 +87,24 @@ const SignUp = () => {
                 required
                 label="CPF"
                 placeholder='000.000.000-00'
-                type="number"
                 id="outlined-start-adornment"
                 sx={{ m: 1, width: '328px', height: '56px' }}
                 InputProps={{
                     startAdornment: <InputAdornment position="start" />,
                 }}
             />
-            <FormControl className='input' fullWidth sx={{ m: 1, width: '328px', height: '56px' }} variant="outlined" >
-                <InputLabel htmlFor="outlined-adornment-password" id="outlined-start-adornment" label="Senha" required InputProps={{
+
+            <TextField
+                fullWidth
+                className='input'
+                required
+                label="Senha"
+                placeholder='Mínimo 6 caracteres'
+                id="outlined-start-adornment"
+                sx={{ m: 1, width: '328px', height: '56px' }}
+                InputProps={{
                     startAdornment: <InputAdornment position="start" />,
-                }}>Senha</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-password"
-                    type={values.showPassword ? 'text' : 'password'}
-                    value={values.password}
-                    onChange={handleChange('password')}
-
-                    endAdornment={
+                    endAdornment:
                         <InputAdornment position="end">
                             <IconButton
                                 aria-label="toggle password visibility"
@@ -158,33 +115,41 @@ const SignUp = () => {
                                 {values.showPassword ? <VisibilityOff /> : <Visibility />}
                             </IconButton>
                         </InputAdornment>
-                    }
-                />
-            </FormControl>
-            <FormControl className='input' fullWidth sx={{ m: 1, width: '328px', height: '56px' }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password" label="Senha" required>Confirmar</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-password"
-                    type={values.showPassword ? 'text' : 'password'}
-                    value={values.confirmPassword}
-                    onChange={handleChange('password')}
-                    endAdornment={
+                }}
+
+                type={values.showPassword ? 'text' : 'password'}
+                value={values.Password}
+                onChange={handleChange('password')}
+            />
+
+            <TextField
+                fullWidth
+                className='input'
+                required
+                label="Confirmar"
+                placeholder='Confirme a senha anterior'
+                id="outlined-start-adornment"
+                sx={{ m: 1, width: '328px', height: '56px' }}
+                InputProps={{
+                    startAdornment: <InputAdornment position="start" />,
+                    endAdornment:
                         <InputAdornment position="end">
                             <IconButton
                                 aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
+                                onClick={handleClickShowConfirmPassword}
                                 onMouseDown={handleMouseDownPassword}
                                 edge="end"
                             >
-                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                {values.showConfirmPassword ? <VisibilityOff /> : <Visibility />}
                             </IconButton>
                         </InputAdornment>
-                    }
-                    label="Senha"
-                />
-            </FormControl>
 
+                }}
 
+                type={values.showConfirmPassword ? 'text' : 'password'}
+                value={values.confirmPassword}
+                onChange={handleChange('confirmPassword')}
+            />
             <button id="button--criar">Criar</button>
         </SignUpStyle>
     )
