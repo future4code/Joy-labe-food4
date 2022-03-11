35ab78b05515764e9704f6a40263a1dee60ef15c
Wanessa Santos <wannsanttos@gmail.com>
2022-03-11 01:03:55 -0300

    página de detalhes do restaurante

--- src/page/ResultPage/ResultPage.js
+++ src/page/ResultPage/ResultPage.js
@@ -1,12 +1,82 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+
+import { Divider, Grid, Typography } from "@mui/material"
+import { Header } from "../../components/Header"
+
+import { api } from "../../api"
+
+import { useLocation } from 'react-router-dom'
 import useProtectedPage from "../../hooks/useProtectedPage"
+import { CardRestaurantDetail } from "../../components/CardRestaurantDetail"
+import { CardProduct } from "../../components/CardProduct"
+import Loading from '../../images/loading.gif'
 
 const ResultPage = () => {
-  useProtectedPage()
+  const [restaurant, setRestaurant] = useState([])
+  const location = useLocation();
+  const restaurantId = location.pathname.replace("/restaurantes/", "");
+  const token = localStorage.getItem("token");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getRestaurantInfo = async () => {
+    setIsLoading(true);
+    try {
+      const response = await api.get(`/restaurants/${restaurantId}`,{
+        headers: {
+          auth: token,
+        },
+      })
+      setRestaurant(response.data.restaurant);
+      setIsLoading(false);
+    } catch (error) {
+      alert(error.message);
+      setIsLoading(false);
+    }
+
+  };
+
+  useEffect(() => {
+    getRestaurantInfo();
+  }, []);
+
+  useProtectedPage();
+
   return (
-    <div>
-      <p>ResultPage</p>
-    </div>
+    <>
+      <Grid style={{ margin: '0 auto' }}>
+        <Header text="Restaurante" icon />
+
+        <Grid style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+          {isLoading ? (
+            <Grid style={{ height: '350px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <img src={Loading} alt="Loading" />
+            </Grid>
+          ) :
+          (
+            <>
+              <CardRestaurantDetail image={restaurant.logoUrl} name={restaurant.name} category={restaurant.category} deliveryTime={restaurant.deliveryTime} shippingValue={restaurant.shipping} address={restaurant.address} />
+
+              <Grid style={{ width: '360px', marginTop: '1rem' }}>
+                <Typography>
+                  Produtos
+                </Typography>
+                <Divider />
+              </Grid>
+
+              <Grid>
+                {restaurant?.products?.map((product) => (
+                  <Grid style={{ marginTop: '1rem' }}>
+                    <CardProduct key={product.id} idProduct={product.id} name={product.name} image={product.photoUrl} price={product.price} description={product.description} restaurantId={restaurantId} />
+                  </Grid>
+                ))}
+              </Grid>
+            </>
+          )}
+
+        </Grid>
+
+      </Grid>
+    </>
   )
 }
 
