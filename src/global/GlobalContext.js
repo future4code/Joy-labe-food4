9196549489075b1c283138c8e868cd3dcafc99d7
Matheus Grativol <matheus_grati@hotmail.com>
2022-03-13 22:49:35 -0300

    conflito

--- src/global/GlobalContext.js
+++ src/global/GlobalContext.js
@@ -1,29 +1,5 @@
 import React from "react";
-// import { api } from "../api";
 
 const GlobalContext = React.createContext({});
 
-// export const GlobalProvider = ({ children }) => {
-
-//   const getRestaurants = useCallback( async () => {
-//     const response = await api.get("/restaurants");
-
-//     return response.data.restaurants;
-//   })
-
-//   return (
-//     <GlobalContext.Provider value={{
-//       getRestaurants
-//     }}>
-//       {children}
-//     </GlobalContext.Provider>
-//   );
-// }
-
 export default GlobalContext
-
-// export function useGlobal() {
-//   const context = useContext(GlobalContext);
-
-//   return context;
-// }
--- src/page/ProfilePage/ProfilePage.js
+++ src/page/ProfilePage/ProfilePage.js
@@ -1,13 +1,95 @@
 import React from "react"
 import useProtectedPage from "../../hooks/useProtectedPage"
+import { Header } from '../../components/Header'
+import Navigation from '../../components/Navigation'
+// import { ProfileStyle } from './styled';
+// import { goToEdit } from "../../routes/coordinator";
+import { useHistory } from "react-router-dom";
+// import Edit from '../../images/edit.png'
+import axios from "axios";
+import { Base_url } from "../../constants/Urls";
+import { useState, useEffect } from 'react';
+// import {
+//   Container,
+//   InfoContainer,
+//   StyledLink,
+//   AddressContainer
+
+// } from './styled';
+
 
 const ProfilePage = () => {
   useProtectedPage()
+  const [user, setUser] = useState({});
+  const [hist, setHist] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    getProfile()
+    getHistory()
+
+  }, [])
+
+  const getProfile = () => {
+
+    axios
+      .get(`${Base_url}/profile`,
+        {
+          headers: {
+            auth: localStorage.getItem('token')
+          }
+        })
+      .then((response) => {
+        setUser(response.data.user)
+      })
+      .catch((err) => {
+        alert(err.data)
+      })
+  }
+
+  console.log(user)
+
+  const getHistory = () => {
+
+    axios
+      .get(`${Base_url}/orders/history`,
+        {
+          headers: {
+            auth: localStorage.getItem('token')
+          }
+        })
+      .then((response) => {
+        console.log(response.data.orders)
+      })
+      .catch((err) => {
+        alert(err.data)
+      })
+  }
+
   return (
     <div>
-      <p>ProfilePage</p>
+      {/* {<Container> /} */}
+
+        <Header text="Meu Perfil" />
+
+        {/* {/ <PersonalInfoContainer> /} */}
+        <p>{user.name}</p>
+        <p>{user.email}</p>
+        <p>{user.cpf}</p>
+
+        {/* {/ <StyledLink to="/edicao - cadastro"> */}
+        {/* <Edit fontSize="24px" /> */}
+      {/* // </StyledLink> /} */}
+
+      {/* {/ </InfoContainer > /} */}
+    {/* // {/</Container >} */}
+
+      <Navigation />
     </div>
-  )
+
+
+
+  );
 }
 
 export default ProfilePage
